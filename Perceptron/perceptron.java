dd0ba1ec477b643419f47006c109251e8426d736
uwecerron <uwe650@hotmail.com>
2013-02-14 17:48:56 -0500

    Softmax Activation function under construction

--- Perceptron/perceptron.java
+++ Perceptron/perceptron.java
@@ -2,86 +2,98 @@
 
 import java.util.*;
 
-class Neuron {
-    private ArrayList<Neuron> inputs;
-    private float weight;
-    private float threshhold;
-    private boolean fired;
+class Node {
 
-    public Neuron (float t) {
-        threshhold = t;
-        fired = false;
-        inputs = new ArrayList<Neuron>();
-    }
+	private float weight;
+	private float threshhold;
+	private boolean Activated;
+	private ArrayList<Node> inputs;
 
-    public void connect (Neuron ... ns) {
-        for (Neuron n : ns) inputs.add(n);
-    }
+	// constructor
+	public Node(float t) {
+		threshhold = t;
+		Activated = false;
+		inputs = new ArrayList<Node>();
+	}
 
-    public void setWeight (float newWeight) {
-        weight = newWeight;
-    }
+	public void setWeight(float newWeight) {
+		weight = newWeight;
+	}
 
-    public void setWeight (boolean newWeight) {
-        weight = newWeight ? 1.0f : 0.0f;
-    }
+	public void setWeight(boolean newWeight) {
+		weight = newWeight ? 1.0f : 0.0f;
+	}
 
-    public float getWeight () {
-        return weight;
-    }
+	public float getWeight() {
+		return weight;
+	}
 
-    public float fire () {
-        if (inputs.size() > 0) {
-            float totalWeight = 0.0f;
-            for (Neuron n : inputs) {
-                n.fire();
-                totalWeight += (n.isFired()) ? n.getWeight() : 0.0f;
-            }
-            fired = totalWeight > threshhold;
-            return totalWeight;
-        }
-        else if (weight != 0.0f) {
-            fired = weight > threshhold;
-            return weight;
-        }
-        else {
-            return 0.0f;
-        }
-    }
+	public void connect(Node... ns) {
+		for (Node n : ns)
+			inputs.add(n);
+	}
 
-    public boolean isFired () {
-        return fired;
-    }
-}
+	// softmax function
+	/*
+     * 
+     * 
+     */
+	public int[] softmax() {
+		Node Magnitude;
+		int[] results = new int[inputs.size()];
+		for (int i = 0; i < inputs.size(); i++) {
+			Magnitude = inputs.get(i);
+		}
 
-public class perceptron { 
-		
-		    public static void main (String [] args) {
-		        Neuron xor = new Neuron(0.5f);
-		        Neuron left = new Neuron(1.5f);
-		        Neuron right = new Neuron(0.5f);
-		        left.setWeight(-1.0f);
-		        right.setWeight(1.0f);
-		        xor.connect(left, right);
+		return results;
+	}
 
-		        for (String val : args) {
-		            Neuron op = new Neuron(0.0f);
-		            op.setWeight(Boolean.parseBoolean(val));
-		            left.connect(op);
-		            right.connect(op);
-		        }
+	// fire transactions are handled here
+	public float pulse() {
+		if (inputs.size() > 0) {
+			float totalWeight = 0.0f;
+			for (Node n : inputs) {
+				n.pulse();
+				totalWeight += (n.isActivated()) ? n.getWeight() : 0.0f;
+			}
+			Activated = totalWeight > threshhold;
+			return totalWeight;
+		} else if (weight != 0.0f) {
+			Activated = weight > threshhold;
+			return weight;
+		} else {
+			return 0.0f;
+		}
+	}
 
-		        xor.fire();
+	public boolean isActivated() {
+		return Activated;
+	}
 
-		        System.out.println("Result: " + xor.isFired());
+}// end Neuron class
 
-		    }
-		
-		    
+public class perceptron {
+
+	public static void main(String[] args) {
+		Node xor = new Node(0.5f);
+		Node left = new Node(1.0f);
+		Node right = new Node(0.5f);
+		left.setWeight(-1.0f);
+		right.setWeight(1.0f);
+		xor.connect(left, right);
+
+		for (String val : args) {
+			Node op = new Node(0.0f);
+			op.setWeight(Boolean.parseBoolean(val));
+			left.connect(op);
+			right.connect(op);
+		}
+
+		 xor.pulse();
 		 
-	
-	
-	
+		System.out.println("Result: " + xor.isActivated());
 
-}
+	}// end main
+
+}// end perceptron
 
